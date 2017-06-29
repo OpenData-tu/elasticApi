a4f45eb69060093db7bcc7f3f343da86b6254d5f
Nico Tasche <nico.tasche@campus.tu-berlin.de>
2017-06-29 10:49:38 +0200

    added gelocation boundingbox filter and time based filtering

--- app.js
+++ app.js
@@ -45,6 +45,26 @@ router.use(function (req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+// Parse location data
+router.use(function (req, res, next) {
+    
+    if(req.query['location']){
+        req.query['location'] = req.query['location'].split(",");
+    }
+    console.log("Parsed Location")
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// Parse time data
+router.use(function (req, res, next) {
+    
+    if(req.query['time']){
+        req.query['time'] = req.query['time'].split(",");
+    }
+    console.log("Parsed Time")
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 router.get('/', function (req, res) {
     // TODO index.html
     let answer = '<h1>Elasticsearch API is running</h1>' +
@@ -96,11 +116,30 @@ router.route('/indices/:indexName')
 
 router.route('/indices/:indexName/bucket/:time/agr/:type')
     .get(function (req, res) {  
+        let timeGte = "new-1h/m"
+        let timeLte = "new/m"
+        if(req.query["time"] && req.query["time"].length === 2){
+            timeGte = req.query["time"][0];
+            timeLte = req.query["time"][1];
 
+        }
         let jsonVar = {        
             index: req.params.indexName,        
             size: 0,    
             body: {
+               
+                 "query": {
+                    "constant_score": {
+                        "filter": {
+                            "range": {
+                                "timestamp": {
+                                    "gte": timeGte,
+                                    "lte": timeLte
+                                }
+                            }
+                        }
+                    }
+                 },
                 sort: [{ "timestamp": { "order": "desc" } }],
                 "aggs": {
                         "agg_per_time": {
@@ -120,7 +159,38 @@ router.route('/indices/:indexName/bucket/:time/agr/:type')
                 
             },
             
-        }      
+        }              
+        if(req.query["location"] && req.query["location"].length === 4){
+            if(!jsonVar.body.query.constant_score.filter["bool"]){
+                let range = jsonVar.body.query.constant_score.filter;
+                jsonVar.body.query.constant_score.filter = {
+                    "bool":{
+                        "must":[]
+                    }
+                }
+                jsonVar.body.query.constant_score.filter.bool.must.push(range);
+            }
+            jsonVar.body.query.constant_score.filter.bool.must.push({       
+                "geo_bounding_box":      {
+                    "location": {
+                        "top_left": {
+                            "lat":  req.query["location"][0],
+                            "lon":  req.query["location"][1],
+                            },
+                        "bottom_right": {
+                            "lat":  req.query["location"][2],
+                            "lon":  req.query["location"][3],
+                        }
+                    }
+                    
+
+                }
+            });
+
+        }
+        console.log(JSON.stringify(jsonVar, null, 2));
+        
+        
         esClient.search(jsonVar)
             .then(function (result) {
                 res.json(result.aggregations.agg_per_time.buckets.map(d =>{
